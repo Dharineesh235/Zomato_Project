c784690098d127978539660def86cc635a1f6b35
Dharineesh235 <dharineesh235@gmail.com>
2023-04-13 17:41:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from './Components/HomePage/Home';
+import Details from './Components/DetailsPage/Details';
+import Filters from './Components/FiltersPage/Filter';
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>,
+  },
+  {
+    path:"/details/:rName",
+    element:<Details/>
+  },
+  {
+    path:"/filters/:pgNo",
+    element:<Filters/>
+  }
+
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    // <Home/>
+      <RouterProvider router={router}/>
+  
   );
 }
 
